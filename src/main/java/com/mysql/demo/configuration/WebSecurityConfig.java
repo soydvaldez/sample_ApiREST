9d4fdeb9ab6f659c056579cb039dd9b2cb4f8405
dvaldez <dvaldez@no-reply.com>
2023-08-24 14:39:28 -0600

    Security: modulo - configuration global scope
    Se agrego un SecurityFilterChain para proteger las rutas de
    accesos no autorizados. Se agrego un mecanismo de
    autenticacion: basic auth que solicita un 'username' y un
    'password' validos para poder consumir los endpoints

---
+++ src/main/java/com/mysql/demo/configuration/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.mysql.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private UserDetails user;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((request) -> {
+                    request.requestMatchers("/orders/**").authenticated();
+                    request.requestMatchers("/products/**").authenticated();
+                    request.requestMatchers("/users/**").authenticated();
+                }).httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    private BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
